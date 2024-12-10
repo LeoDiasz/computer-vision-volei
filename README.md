ee7ef7e66d8948cfd2dce4566e469db43a8fab73
leonardo.dias <dias_leonardo@terceiros.sicredi.com.br>
2024-12-10 00:14:34 -0300

    feat: add detect objects persons and ball

--- README.md
+++ README.md
@@ -1,27 +1,54 @@
 # Visão Computacional para Contador de Pontos numa partida de Volei
 
 Este projeto utiliza técnicas de visão computacional para verificar quantos pontos cada equipe realizou. Ele verifica cada imagem do video, observando se a bola caiu no chão, se sim, conta ponto para equipe.
+Verifica também, quantas pessoas estão na quadra
 
 ## Etapas
 - Bola caiu no chão
 - No campo direito, conta ponto para equipe direita, no campo esquerdo, para equipe a esquerda.
+- Verificar Pessoas na quadra
 
 
 ## Como é Feito
 - Os pontos de referencia são os dois campos, um do lado esquerdo e o outro no direito. 
 - Verifica em pixels brancos, se aumentou a quantidade em um dos campos, se sim, conta ponta para a equipe.
 
+## Necessário
+- Arquivo de configuração: yolov3.cfg = https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg
+- Arquivo de pesos: yolov3.weights = https://pjreddie.com/media/files/yolov3-tiny.weights
+- Arquivo de nomes das classes: coco.names = https://github.com/pjreddie/darknet/blob/master/data/coco.names
 
-## Função `processImage`
+## Funções 
+
+### Função `processImage`
 
 A função `processImage` é responsável por tratar o frame, transformando em cores brancas e pretas, e retornando uma imagem dilatada no final
 
 
-## Função `verifiyFields`
+### Função `verifiyFields`
 
 A função `verifyFields` verifica se aumentou a quantidade de pixels brancos em cada campo, se sim, conta no scoreBoard e retorna o placar no final
 
 
-## Função `showScoreboard`
+### Função `showScoreboard`
 
 A função `showScoreboard` tem o papel de mostrar o resultado final do placar.
+
+### Função `loadPretainedModel`
+
+A função `loadPretainedModel` Carrega o modelo pra utilizar pra identificar os objetos
+
+### Função `preProcessFrame`
+
+A função `preProcessFrame` pre processa a imagem
+
+### Função `detectObject`
+
+A função `detectObject` Identifica o objeto
+
+### Função `drawDetections`
+
+A função `drawDetections` tem o papel de desenhar os retangulos por volta do objeto
+
+
+
--- main.py
+++ main.py
@@ -7,6 +7,17 @@ teamTwo = 0
 fields = [[0,350,450,200], [500,350,500,200]]
 DELAY = 50
 
+TINY = False
+
+fileCfg = "yolov3{}.cfg".format("-tiny" if TINY else "")
+fileWheights = "yolov3{}.weights".format("-tiny" if TINY else "")
+fileClass = "coco{}.names".format("-tiny" if TINY else "")
+
+with open(fileClass, "r") as file:
+    CLASS = [row.strip() for row in file.readlines()]
+
+# Gerar cores diferentes para cada classe
+COLORS = np.random.uniform(0, 255, size=(len(CLASS), 3))
 
 def processImage(img):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY);
@@ -48,14 +59,79 @@ def verifyFields(img, imgDilated, fields):
 def showScoreboard(img, scoreBoardFields):
     cv2.rectangle(img, (90, 0), (415, 60), (0, 0, 0), -1)
     cv2.putText(img, 'Placar: {}/{}'.format(scoreBoardFields[0], scoreBoardFields[1]), (100, 45), cv2.FONT_HERSHEY_DUPLEX, 1.5, (255, 255, 255), 5)
-    
+
+def loadPretainedModel():
+    model = cv2.dnn.readNetFromDarknet(fileCfg, fileWheights)
+    model.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+    model.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+    if model.empty():
+        raise IOError("Não foi possivel carregar o modelo!")
+
+    return model 
+
+def preProcessFrame(img):
+    blob = cv2.dnn.blobFromImage(img, scalefactor=1/255.0, size=(416, 416), swapRB=True, crop=False)
+    return blob
+
+def detectObject(img, model):
+    blob = preProcessFrame(img)
+    model.setInput(blob)
+    layersNames = model.getLayerNames()
+    layersOutput = [layersNames[i - 1] for i in model.getUnconnectedOutLayers()]
+    outputs = model.forward(layersOutput)
+    return outputs
+
+
+def drawDetections(img, detections, limiar=0.5):
+    (height, width) = img.shape[:2]
+    boxs = []
+    trusts = []
+    idsClass = []
+
+    for output in detections:
+        for detection in output:
+            points = detection[5:]
+            idClass = np.argmax(points)
+            trust = points[idClass]
+            if trust > limiar:
+                box = detection[0:4] * np.array([width, height, width, height])
+                (centerX, centerY, widthBox, heightBox) = box.astype("int")
+                x = int(centerX - (widthBox / 2))
+                y = int(centerY - (heightBox / 2))
+
+                boxs.append([x, y, int(widthBox), int(heightBox)])
+                trusts.append(float(trust))
+                idsClass.append(idClass)
+
+    indices = cv2.dnn.NMSBoxes(boxs, trusts, limiar, limiar - 0.1)
+    if len(indices) > 0:
+        for i in indices.flatten():
+            (x, y) = (boxs[i][0], boxs[i][1])
+            (widthBox, heightBox) = (boxs[i][2], boxs[i][3])
+            color = [int(c) for c in COLORS[idsClass[i]]]
+            cv2.rectangle(img, (x, y), (x + widthBox, y + heightBox), color, 2)
+            text = f"{CLASS[idsClass[i]]}: {trusts[i]:.2f}"
+            cv2.putText(img, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 def main():
     video_path = "volei1.mp4";
     video = cv2.VideoCapture(video_path);
+    model = loadPretainedModel()
 
     if not video.isOpened():
         print(f"Erro ao abrir o video: {video_path}")
         return
+    
+
+    limiarTrust = 0.5
+
+    def ajustLimiar(value):
+        nonlocal limiarTrust
+        limiarTrust = value / 100
+
+
+    if TINY:
+        cv2.createTrackbar('Limiar de Confiança', 'Detecta Objetos', int(limiarTrust * 100), 100, ajustLimiar)
 
     while True:
         check, img = video.read();
@@ -64,6 +140,9 @@ def main():
             break
         
         imgDilated = processImage(img);
+
+        detections = detectObject(img, model)
+        drawDetections(img, detections, limiarTrust)
 
         scoreBoardFields = verifyFields(img, imgDilated[0], fields)
 
@@ -74,11 +153,7 @@ def main():
         cv2.namedWindow('Processamento', cv2.WINDOW_NORMAL)
         cv2.resizeWindow('Processamento', 350, 200)
         cv2.imshow('Processamento', imgDilated[0])
-        
-        cv2.namedWindow('Passo', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('Passo', 350, 200)
-        cv2.imshow('Passo', imgDilated[1])
-
+      
         if cv2.waitKey(DELAY) == ord('q'):
             break
 
